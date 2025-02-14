e3f02794fe4816d23854767b7ba8358be4946bfb
jcjoo <joaovafc4@gmail.com>
2025-02-14 19:56:16 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import './App.css'
 import { Turma } from './pages/turma/Turma'
+
+function Content() {
+  return <div className='h-96 overflow-auto'>
+    {[...new Array(1000)].map((item, i) =>
+      <p key={item} className={i % 2 === 0 ? 'bg-slate-300' : 'bg-slate-100'}> Bolo {i} </p>
+    )}
+  </div >
+}
+
 function App() {
   return (
     <>
       <Turma></Turma>
+      <Content />
     </>
   )
 }
